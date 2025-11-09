03adb4fe36880ceb629a392f4acbef1a88dc4824
Mion <deniskin741@gmail.com>
2025-11-09 13:23:37 +0300

    UI/UX boost.

--- src/features/components/SearchBooks/SearchBooks.tsx
+++ src/features/components/SearchBooks/SearchBooks.tsx
@@ -41,7 +41,24 @@ export default function BooksList() {
               placeholder='Search by title, author, genre...'
               className='search-books__input'
             />
-            <span className='search-books__icon'>🔍</span>
+            <span
+              style={{
+                cursor: !searchQuery.trim() ? 'default' : 'pointer',
+                opacity: !searchQuery.trim() ? '0.5' : '1',
+              }}
+              className='search-books__icon'
+              onClick={() => handleSearch(searchQuery)}
+            >
+              <svg width='20' height='20' viewBox='0 0 24 24' fill='none'>
+                <path
+                  d='M21 21L16.514 16.506L21 21ZM19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
+              </svg>
+            </span>
           </div>
 
           <div className='search-books__actions'>
