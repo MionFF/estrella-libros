6093986ab088abbe4d69ee5229de27d0b39a0fac
Mion <deniskin741@gmail.com>
2025-11-23 15:42:14 +0300

    fix: screenshots in includeAssets.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'screenshot-desktop.png', 'screenshot-mobile.png'],
       manifest: {
         name: 'Estrella Libros',
         short_name: 'Estrella',
